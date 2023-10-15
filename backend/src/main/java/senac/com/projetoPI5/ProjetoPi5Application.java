469e1c1b1685f2dac0e54e47a3343734c007534e
Luciano <luc.merce@hotmail.com>
2023-10-15 13:24:55 -0300

    Subindo projeto inicial

---
+++ backend/src/main/java/senac/com/projetoPI5/ProjetoPi5Application.java
@@ -0,0 +1,13 @@
+package senac.com.projetoPI5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPi5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPi5Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/senac/com/projetoPI5/ProjetoPi5ApplicationTests.java
@@ -0,0 +1,13 @@
+package senac.com.projetoPI5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPi5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
