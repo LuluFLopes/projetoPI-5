52361558e90f52a1a4e710c086a43e477b67da4a
Luciano <luc.merce@hotmail.com>
2023-10-15 23:57:09 -0300

    Refatorando service e criando as outras

--- backend/src/main/java/senacsp/com/ProjetoPI5/repository/FuncionarioRepository.java
+++ backend/src/main/java/senacsp/com/ProjetoPI5/repository/FuncionarioRepository.java
@@ -3,5 +3,5 @@ package senacsp.com.ProjetoPI5.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import senacsp.com.ProjetoPI5.model.Funcionario;
 
-public interface AtendenteRepository extends JpaRepository<Funcionario, Integer> {
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Integer> {
 }
