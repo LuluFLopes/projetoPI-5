3c7b396b79dd3e319c2c72c241b66356d8d185f9
Luciano <luc.merce@hotmail.com>
2023-10-16 02:23:00 -0300

    Ajustando funcionario

--- backend/src/main/java/senacsp/com/ProjetoPI5/model/Funcionario.java
+++ backend/src/main/java/senacsp/com/ProjetoPI5/model/Funcionario.java
@@ -2,7 +2,18 @@ package senacsp.com.ProjetoPI5.model;
 
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
+import senacsp.com.ProjetoPI5.model.enumeradores.Cargo;
 
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public class Funcionario extends Pessoa {
+
+    private Cargo cargo;
+
+    public Cargo getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(Cargo cargo) {
+        this.cargo = cargo;
+    }
 }
