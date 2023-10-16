d741c579f3bdd738f0c1d290b118e5440c701a7e
Luciano <luc.merce@hotmail.com>
2023-10-16 03:20:49 -0300

    Ajustando query andamento

--- backend/src/main/java/senacsp/com/ProjetoPI5/repository/AgendamentoRepository.java
+++ backend/src/main/java/senacsp/com/ProjetoPI5/repository/AgendamentoRepository.java
@@ -1,7 +1,14 @@
 package senacsp.com.ProjetoPI5.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import senacsp.com.ProjetoPI5.model.Agendamento;
+import senacsp.com.ProjetoPI5.model.enumeradores.Andamento;
 
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Integer> {
+    @Modifying
+    @Query("UPDATE Agendamento a SET a.andamento = :andamento WHERE a.id = :id")
+    void ajustarStatus(@Param("andamento") Andamento andamento, @Param("id") Integer id);
 }
