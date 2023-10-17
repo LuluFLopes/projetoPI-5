a922c722b745916c075b416e61022e450831a213
Luciano <luc.merce@hotmail.com>
2023-10-16 21:33:56 -0300

    Adicionando status na classe de especialização

--- backend/src/main/java/senacsp/com/ProjetoPI5/model/Especializacao.java
+++ backend/src/main/java/senacsp/com/ProjetoPI5/model/Especializacao.java
@@ -4,13 +4,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import senacsp.com.ProjetoPI5.model.enumeradores.Status;
 
 @Entity
 public class Especializacao {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    int id;
-    String descricao;
+    private int id;
+    private String descricao;
+    private Status status;
 
     public int getId() {
         return id;
