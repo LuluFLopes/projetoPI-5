36dcf07c519d7c18ca79562fe22facd8dac4bf12
FernandoMN <fernando31.mnascimento@gmail.com>
2023-11-25 21:16:12 -0300

    Ajustes e conexao feitas nas telas de cadastro de paciente e funcionario. Problemas no envio do cadastro de medicos.

--- frontend/src/View/CadastroFuncionario.vue
+++ frontend/src/View/CadastroFuncionario.vue
@@ -1,23 +1,10 @@
 <template>
+  <div>
   <v-container>
     <v-form @submit="cadastrarCliente">
       <v-text-field
           v-model="funcionario.nome"
           label="Nome"
-          required
-          color="teal darken-3"
-          outlined
-      ></v-text-field>
-      <v-text-field
-          v-model="funcionario.email"
-          label="E-mail"
-          required
-          color="teal darken-3"
-          outlined
-      ></v-text-field>
-      <v-text-field
-          v-model="funcionario.telefone"
-          label="Telefone"
           required
           color="teal darken-3"
           outlined
@@ -30,6 +17,129 @@
           outlined
           placeholder="Ex: 000.000.000-00"
       ></v-text-field>
+      <v-menu
+          ref="menu"
+          v-model="menu"
+          :close-on-content-click="false"
+          transition="scale-transition"
+          offset-y
+          min-width="auto"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-text-field
+              v-model="funcionario.dataNascimento"
+              label="Data de nascimento"
+              required
+              color="teal darken-3"
+              outlined
+              prepend-icon="mdi-calendar"
+              readonly
+              v-bind="attrs"
+              v-on="on"
+          ></v-text-field>
+        </template>
+        <v-date-picker
+            v-model="funcionario.dataNascimento"
+            header-color="#7ececa"
+            @change="save"
+        ></v-date-picker>
+      </v-menu>
+      <v-text-field
+          v-model="funcionario.endereco.logradouro"
+          label="Logradouro"
+          required
+          color="teal darken-3"
+          outlined
+      ></v-text-field>
+      <v-text-field
+          v-model="funcionario.endereco.numero"
+          label="Numero"
+          required
+          color="teal darken-3"
+          outlined
+          type="number"
+      ></v-text-field>
+      <v-text-field
+          v-model="funcionario.endereco.complemento"
+          label="Complemento"
+          required
+          color="teal darken-3"
+          outlined
+      ></v-text-field>
+      <v-text-field
+          v-model="funcionario.endereco.cep"
+          label="CEP"
+          required
+          color="teal darken-3"
+          outlined
+      ></v-text-field>
+      <v-text-field
+          v-model="funcionario.endereco.bairro"
+          label="Bairro"
+          required
+          color="teal darken-3"
+          outlined
+      ></v-text-field>
+      <v-text-field
+          v-model="funcionario.endereco.cidade"
+          label="cidade"
+          required
+          color="teal darken-3"
+          outlined
+      ></v-text-field>
+      <v-select
+          v-model="funcionario.endereco.uf"
+          :items="ufs"
+          label="Uf"
+          required
+          color="teal darken-3"
+          outlined
+      ></v-select>
+      <v-text-field
+          v-model="funcionario.contato.email"
+          label="E-mail"
+          required
+          color="teal darken-3"
+          outlined
+          type="email"
+      ></v-text-field>
+      <v-text-field
+          v-model="funcionario.contato.telefone"
+          label="Telefone"
+          required
+          color="teal darken-3"
+          outlined
+      ></v-text-field>
+      <v-text-field
+          v-model="funcionario.login.usuario"
+          label="Login Usuario"
+          required
+          color="teal darken-3"
+          outlined
+      ></v-text-field>
+      <v-text-field
+          v-model="funcionario.login.senha"
+          label="Login Senha"
+          required
+          color="teal darken-3"
+          outlined
+      ></v-text-field>
+      <v-select
+          v-model="funcionario.status"
+          :items="statuses"
+          label="Status"
+          required
+          color="teal darken-3"
+          outlined
+      ></v-select>
+      <v-select
+          v-model="funcionario.genero"
+          :items="generos"
+          label="Genero"
+          required
+          color="teal darken-3"
+          outlined
+      ></v-select>
       <v-select
           v-model="funcionario.tipoFuncionario"
           :items="tipos"
@@ -40,33 +150,54 @@
           @change="verificarTipo"
       ></v-select>
       <v-text-field
-          v-model="funcionario.medicoEspecialidade"
-          label="Especialidade"
+          v-model="funcionario.crm"
+          label="CRM"
           color="teal darken-3"
           outlined
-          v-if="funcionario.tipoFuncionario === 'Médico'"
-      ></v-text-field>
-      <v-text-field
-          v-model="funcionario.medicoDisponibilidade"
-          label="Disponibilidade"
-          color="teal darken-3"
-          outlined
-          v-if="funcionario.tipoFuncionario === 'Médico'"
-      ></v-text-field>
-      <v-text-field
-          v-model="funcionario.funcionarioArea"
-          label="Área"
-          color="teal darken-3"
-          outlined
-          v-if="funcionario.tipoFuncionario === 'Funcionário'"
+          v-if="funcionario.tipoFuncionario === 'MEDICO'"
       ></v-text-field>
       <v-select
-          v-model="funcionario.funcionarioHorario"
-          :items="horarios"
-          label="Horário"
+          v-model="funcionario.especializacaoId"
+          :items="opcoesDoSelect"
+          item-text="descricao"
+          item-value= "id"
+          label="Especializacao"
           color="teal darken-3"
           outlined
-          v-if="funcionario.tipoFuncionario === 'Funcionário'"
+          type="number"
+          v-if="funcionario.tipoFuncionario === 'MEDICO'"
+      >
+        <template v-slot:item="{ item } ">
+          {{ item.id }}
+          {{ item.descricao }}
+          {{ item.status }}
+        </template>
+      </v-select>
+      <v-select
+          v-model="funcionario.unidadeId"
+          :items="opcoesDaUnidade"
+          item-text="descricao"
+          item-value="id"
+          label="Unidade"
+          color="teal darken-3"
+          outlined
+          type="number"
+          v-if="funcionario.tipoFuncionario === 'MEDICO'"
+      >
+        <template v-slot:item1="{ item1 } ">
+          {{ item1.id }}
+          {{ item1.descricao }}
+          {{ item1.status }}
+        </template>
+      </v-select>
+      <v-select
+          v-model="funcionario.cargo"
+          :items="cargos"
+          label="Cargo"
+          color="teal darken-3"
+          outlined
+          type="number"
+          v-if="funcionario.tipoFuncionario === 'FUNCIONARIO'"
       ></v-select>
 
       <v-row>
@@ -76,6 +207,14 @@
       </v-row>
     </v-form>
   </v-container>
+  <v-alert class="alerta-total"
+           v-if="alertaLigado"
+           dismissible
+           elevation="24"
+           :type="tipoAlerta"
+  >{{ this.msgAlerta }}
+  </v-alert>
+  </div>
 </template>
 
 
@@ -87,59 +226,149 @@ export default {
     return {
       funcionario: {
         nome: '',
-        email: '',
-        telefone: '',
         cpf: '',
-        tipoFuncionario: null,
+        dataNascimento: '',
+        endereco: {
+          logradouro: '',
+          numero: '',
+          complemento: '',
+          cep: '',
+          bairro: '',
+          cidade: '',
+          uf: ''
+        },
+        contato: {
+          email: '',
+          telefone: ''
+        },
+        login: {
+          usuario: '',
+          senha: ''
+        },
+        status: '',
+        genero: '',
       },
-      tipos: ['Médico','Funcionário'],
-      horarios: ['08:00 - 17:00', '16:00 - 01:00', '24:00 - 09:00'],
+      tipos: [
+        {
+          text:'Médico',
+          value:'MEDICO'
+        },
+        {
+          text:'Funcionário',
+          value:'FUNCIONARIO'
+        }
+      ],
+      generos: [
+        'MASCULINO',
+        'FEMININO',
+        'PREFIRO_NAO_INFORMAR'
+      ],
+      statuses: [
+        'ATIVO',
+        'INATIVO'
+      ],
+      ufs: [
+        'AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG',
+        'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'
+      ],
+      cargos: [
+        'ADMINISTRADOR',
+        'ATENDENTE'
+      ],
+      opcoesDoSelect: [],
+      opcoesDaUnidade: [],
+      alertaLigado: false,
+      tipoAlerta: '',
+      msgAlerta: '',
     };
   },
   methods: {
     async cadastrarCliente() {
-      if (this.funcionario.tipoFuncionario === 'Médico') {
+      if (this.funcionario.tipoFuncionario === 'MEDICO') {
         try {
-          alert("Deu certo o cadastro do medico");
+          this.gerarAlerta('info', "sucesso em criar cadastro de medico", 3);
+          alert(this.funcionario.especializacao.id);
           await axios.post('http://localhost:8081/medicos/cadastrar', this.funcionario);
 
-          this.limparFormulario();
+          //this.limparFormulario();
         } catch (ex) {
-          alert("Não foi possível fazer o cadastro do médico!");
+          this.gerarAlerta('error', 'Não foi possível realizar o cadastro do médico!', 3);
           console.log(ex.message);
         }
-      } else if (this.funcionario.tipoFuncionario === 'Funcionário'){
+      } else if (this.funcionario.tipoFuncionario === 'FUNCIONARIO'){
         try {
-          alert("Deu certo o cadastro do funcionario");
+          this.gerarAlerta('info', "sucesso em criar cadastro de funcionario", 3);
           await axios.post('http://localhost:8081/funcionarios/cadastrar', this.funcionario);
 
-          this.limparFormulario();
+          //this.limparFormulario();
         } catch (ex) {
-          alert("Não foi possível fazer o cadastro do funcionário!");
+          this.gerarAlerta('error', 'Não foi possível realizar o cadastro do funcionário!', 3);
           console.log(ex.message);
         }
       }
 
     },
     verificarTipo() {
-      if (this.funcionario.tipoFuncionario !== 'Médico') {
-        this.funcionario.medicoEspecialidade = '';
-        this.funcionario.medicoDisponibilidade = '';
+      if (this.funcionario.tipoFuncionario === 'MEDICO') {
+        this.funcionario.crm = '';
+        this.funcionario.especializacao = {
+          id: this.funcionario.especializacaoId
+      };
+        this.funcionario.unidade = {
+          id: this.funcionario.unidadeId
+        };
       }
-      if (this.funcionario.tipoFuncionario !== 'Funcionário'){
-        this.funcionario.funcionarioArea = '';
-        this.funcionario.funcionarioHorario = null;
+      if (this.funcionario.tipoFuncionario === 'FUNCIONARIO'){
+        this.funcionario.cargo = '';
       }
     },
     limparFormulario(){
       this.funcionario.nome = '';
-      this.funcionario.email = '';
-      this.funcionario.telefone = '';
       this.funcionario.cpf = '';
-      this.funcionario.plano = '';
-      this.funcionario.cartaoPlano = '';
-    }
+      this.funcionario.dataNascimento = '';
+      this.funcionario.endereco = '';
+      this.funcionario.contato = '';
+      this.funcionario.login.usuario = '';
+      this.funcionario.login.senha = '';
+      this.funcionario.status = '';
+      this.funcionario.genero = '';
+
+    },
+    save (date) {
+      this.$refs.menu.save(date)
+    },
+    async fetchData() {
+      try {
+        const response = await axios.get('http://localhost:8081/especializacao/listar');
+        this.opcoesDoSelect = response.data;
+      } catch (error) {
+        this.gerarAlerta('error', 'Erro ao recuperar os dados das especializacoes', 3);
+      }
+    },
+    async pegarUnidade(){
+      try{
+        const response2 = await axios.get('http://localhost:8081/unidades/listar');
+        this.opcoesDaUnidade = response2.data;
+      } catch(error) {
+        this.gerarAlerta('error', 'Erro ao recuperar os dados das unidades', 3);
+      }
+    },
+    gerarAlerta(tipoDeAlerta, mensagem, segundosParaFechar) {
+      this.tipoAlerta = tipoDeAlerta;
+      this.msgAlerta = mensagem;
+      this.alertaLigado = true;
+
+      setTimeout(() => {
+        this.alertaLigado = false;
+      }, segundosParaFechar * 1000);
+    },
   },
+  mounted(){
+    this.fetchData();
+    this.pegarUnidade();
+  }
+
+
 };
 </script>
 
@@ -159,5 +388,11 @@ v-icon {
 v-btn {
   background-color: #45b5c4;
   color: white;
+}
+
+.alerta-total {
+  position: absolute;
+  top: 1vh;
+  left: 3vw;
 }
 </style>
--- frontend/src/View/CadastroPaciente.vue
+++ frontend/src/View/CadastroPaciente.vue
@@ -1,24 +1,10 @@
 <template>
+  <div>
   <v-container>
     <v-form @submit="cadastrarCliente">
       <v-text-field
           v-model="cliente.nome"
           label="Nome"
-          required
-          color="teal darken-3"
-          outlined
-
-      ></v-text-field>
-      <v-text-field
-          v-model="cliente.email"
-          label="E-mail"
-          required
-          color="teal darken-3"
-          outlined
-      ></v-text-field>
-      <v-text-field
-          v-model="cliente.telefone"
-          label="Telefone"
           required
           color="teal darken-3"
           outlined
@@ -31,31 +17,132 @@
           outlined
           placeholder="Ex: 000.000.000-00"
       ></v-text-field>
-      <v-select
-          v-model="cliente.plano"
-          :items="planosSaude"
-          label="Plano de Saúde"
+      <v-menu
+          ref="menu"
+          v-model="menu"
+          :close-on-content-click="false"
+          transition="scale-transition"
+          offset-y
+          min-width="auto"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-text-field
+              v-model="cliente.dataNascimento"
+              label="Data de nascimento"
+              required
+              color="teal darken-3"
+              outlined
+              prepend-icon="mdi-calendar"
+              readonly
+              v-bind="attrs"
+              v-on="on"
+          ></v-text-field>
+        </template>
+        <v-date-picker
+            v-model="cliente.dataNascimento"
+            header-color="#7ececa"
+            @change="save"
+        ></v-date-picker>
+      </v-menu>
+      <v-text-field
+          v-model="cliente.endereco.logradouro"
+          label="Logradouro"
           required
           color="teal darken-3"
           outlined
-          @change="verificarOutroPlano"
+      ></v-text-field>
+      <v-text-field
+          v-model="cliente.endereco.numero"
+          label="Numero"
+          required
+          color="teal darken-3"
+          outlined
+          type="number"
+      ></v-text-field>
+      <v-text-field
+          v-model="cliente.endereco.complemento"
+          label="Complemento"
+          required
+          color="teal darken-3"
+          outlined
+      ></v-text-field>
+      <v-text-field
+          v-model="cliente.endereco.cep"
+          label="CEP"
+          required
+          color="teal darken-3"
+          outlined
+      ></v-text-field>
+      <v-text-field
+          v-model="cliente.endereco.bairro"
+          label="Bairro"
+          required
+          color="teal darken-3"
+          outlined
+      ></v-text-field>
+      <v-text-field
+          v-model="cliente.endereco.cidade"
+          label="cidade"
+          required
+          color="teal darken-3"
+          outlined
+      ></v-text-field>
+      <v-select
+          v-model="cliente.endereco.uf"
+          :items="ufs"
+          label="Uf"
+          required
+          color="teal darken-3"
+          outlined
       ></v-select>
       <v-text-field
-          v-model="cliente.outroPlano"
-          label="Especificar Outro Plano"
-          color="teal darken-3"
-          outlined
-          class="custom-input"
-          v-if="cliente.plano === 'Outro'"
-      ></v-text-field>
-      <v-text-field
-          v-model="cliente.cartaoPlano"
-          label="Número do Cartão do Plano de Saúde"
+          v-model="cliente.contato.email"
+          label="E-mail"
           required
           color="teal darken-3"
           outlined
-          placeholder="Ex: 00000-0000-00000"
+          type="email"
       ></v-text-field>
+      <v-text-field
+          v-model="cliente.contato.telefone"
+          label="Telefone"
+          required
+          color="teal darken-3"
+          outlined
+          type="phone"
+          placeholder="(##) #####-####"
+      ></v-text-field>
+      <v-text-field
+          v-model="cliente.login.usuario"
+          label="Login Usuario"
+          required
+          color="teal darken-3"
+          outlined
+      ></v-text-field>
+      <v-text-field
+          v-model="cliente.login.senha"
+          label="Login Senha"
+          required
+          color="teal darken-3"
+          outlined
+      ></v-text-field>
+      <v-select
+          v-model="cliente.status"
+          :items="statuses"
+          label="Status"
+          required
+          color="teal darken-3"
+          outlined
+      ></v-select>
+      <v-select
+          v-model="cliente.genero"
+          :items="generos"
+          label="Genero"
+          required
+          color="teal darken-3"
+          outlined
+      ></v-select>
+
 
       <v-row>
       <v-btn type="submit" color="teal darken-2">Cadastrar Cliente</v-btn>
@@ -64,6 +151,14 @@
       </v-row>
     </v-form>
   </v-container>
+  <v-alert class="alerta-total"
+           v-if="alertaLigado"
+           dismissible
+           elevation="24"
+           :type="tipoAlerta"
+  >{{ this.msgAlerta }}
+  </v-alert>
+  </div>
 </template>
 
 
@@ -76,9 +171,27 @@ export default {
       url: 'http://localhost:8081/pacientes/cadastrar',
       cliente: {
         nome: '',
-        email: '',
-        telefone: '',
         cpf: '',
+        dataNascimento: '',
+        endereco: {
+          logradouro: '',
+          numero: '',
+          complemento: '',
+          cep: '',
+          bairro: '',
+          cidade: '',
+          uf: ''
+        },
+        contato: {
+          email: '',
+          telefone: ''
+        },
+        login: {
+          usuario: '',
+          senha: ''
+        },
+        status: '',
+        genero: '',
         plano: null,
         cartaoPlano: ''
       },
@@ -89,18 +202,30 @@ export default {
         'Amil Assistência Médica',
         'Central Nacional Unimed',
         'Outro',
+      ],
+      generos: [
+        'MASCULINO',
+        'FEMININO',
+        'PREFIRO_NAO_INFORMAR'
+      ],
+      statuses: [
+        'ATIVO',
+        'INATIVO'
+      ],
+      ufs: [
+        'AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG',
+        'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'
       ]
     };
   },
   methods: {
     async cadastrarCliente() {
       try {
-        alert("Deu certo o cadastro do paciente");
+        this.gerarAlerta('info', 'Deu certo o cadastro do paciente', 3);
         await axios.post(this.url, this.cliente);
         this.limparFormulario();
       } catch (ex) {
-        alert("Não foi possível fazer o cadastro!");
-        console.log(ex.message);
+        this.gerarAlerta('error', 'Não foi possível realizar o cadastro', 3);
       }
     },
     verificarOutroPlano() {
@@ -110,19 +235,38 @@ export default {
     },
     limparFormulario(){
       this.cliente.nome = '';
-      this.cliente.email = '';
-      this.cliente.telefone = '';
       this.cliente.cpf = '';
+      this.cliente.dataNascimento = '';
+      this.cliente.endereco = '';
+      this.cliente.contato = '';
+      this.cliente.login = '';
+      this.cliente.status = '';
+      this.cliente.genero = '';
       this.cliente.plano = '';
       this.cliente.cartaoPlano = '';
-    }
+    },
+    save (date) {
+      this.$refs.menu.save(date)
+    },
+    gerarAlerta(tipoDeAlerta, mensagem, segundosParaFechar) {
+      this.tipoAlerta = tipoDeAlerta;
+      this.msgAlerta = mensagem;
+      this.alertaLigado = true;
+
+      setTimeout(() => {
+        this.alertaLigado = false;
+      }, segundosParaFechar * 1000);
+    },
   },
+
+
 };
 </script>
 
 <style>
 v-container {
   background-color: #c7ede8;
+  display: flex;
 }
 
 label {
@@ -137,4 +281,15 @@ v-btn {
   background-color: #45b5c4;
   color: white;
 }
+
+main {
+  margin-bottom: 70px;
+}
+
+.alerta-total {
+  position: absolute;
+  top: 1vh;
+  left: 3vw;
+}
+
 </style>
